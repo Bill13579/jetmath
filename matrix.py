502ea5623fd9f2343686c06ca1f3637c86bf01b4
Bill Kudo <bluesky42624@gmail.com>
2018-06-10 15:59:21 +0900

    Switched from numpy.array to array.array

--- matrix.py
+++ matrix.py
@@ -2,10 +2,9 @@ from array import array as _
 
 class matrix:
     def __init__(self, array):
-        array = _("f", array)
         self.__check_matrix(array)
         self.__init_matrix(array)
-        self.__matrix = array
+        self.__matrix = _("f", [c for r in array for c in r])
 
     def __check_matrix(self, array):
         if len(array) <= 0:
@@ -22,18 +21,29 @@ class matrix:
     
     def __getitem__(self, pos):
         r,c = pos
-        return self.__matrix[r][c]
+        if r >= self.shape[0] or c >= self.shape[1]:
+            raise IndexError("matrix index out of bound")
+        return self.__matrix[c+r*self.shape[1]]
     
     def __setitem__(self, pos, data):
         r,c = pos
-        self.__matrix[r][c] = data
+        if r >= self.shape[0] or c >= self.shape[1]:
+            raise IndexError("matrix index out of bound")
+        self.__matrix[c+r*self.shape[1]] = data
     
     def __str__(self):
-        string = "matrix("
-        m = [[str(c) for c in r] for r in self.__matrix]
-        m = [str(r) for r in self.__matrix]
-        string += "\n       ".join(m)
-        string += ")"
+        string = "matrix( "
+        m = [str(i) for i in self.__matrix]
+        length = max([len(i) for i in m])
+        space = 2
+        m = []
+        for i in range(0, len(self.__matrix), self.shape[1]):
+            r = self.__matrix[i:i+self.shape[1]]
+            tmp = [str(i)+(length-len(str(i))+space)*" " for i in r[:-1]]
+            tmp.append(str(r[-1]))
+            m.append("".join(tmp))
+        string += "\n        ".join(m)
+        string += " )"
         return string
     
     def __add__(self, y):
@@ -66,7 +76,11 @@ class matrix:
         return new
     
     def tolist(self):
-        return self.__matrix.tolist()
+        l = []
+        for i in range(0, len(self.__matrix), self.shape[1]):
+            r = self.__matrix[i:i+self.shape[1]]
+            l.append(r.tolist())
+        return l
     
     @staticmethod
     def transpose(x):
