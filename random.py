5a0ea452228eb28dd3c7d608714e367fd23b2622
Bill Kudo <bluesky42624@gmail.com>
2018-06-16 19:08:27 +0900

    Added randd, randruns, randrun, and choice. Created object RunResult

--- random.py
+++ random.py
@@ -11,3 +11,29 @@ def rand(rows, cols):
             m[r,c] = random.random()
     return m
 
+def randd():
+    return random.random()
+
+def randruns(p, s, f, params=(), iterations=1):
+    results = []
+    for i in range(iterations):
+        results.append(randrun(p, s, f, params))
+    return results
+
+def randrun(p, s, f, params=()):
+    result = None
+    if randd() < p:
+        result = RunResult(s(*params), True)
+    else:
+        result = RunResult(f(*params), False)
+    return result
+
+def choice(seq):
+    return random.choice(seq)
+
+class RunResult:
+    __slots__ = ("return_value", "special")
+    def __init__(self, return_value, special):
+        self.return_value = return_value
+        self.special = special
+
