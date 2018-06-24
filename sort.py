17c081deca9c99662543933c7dcef4eb45246336
Bill Kudo <bluesky42624@gmail.com>
2018-06-24 19:58:47 +0900

    Combined reverse_quicksort and quicksort

--- sort.py
+++ sort.py
@@ -1,6 +1,6 @@
 from jetm import random, math
 
-def quicksort(arr, get_comparable_value=math.identity):
+def quicksort(arr, get_comparable_value=math.identity, reverse=False):
     if len(arr) == 0 or len(arr) == 1:
         return arr
     pivit = random.randint(0, len(arr)-1)
@@ -12,19 +12,8 @@ def quicksort(arr, get_comparable_value=math.identity):
             left.append(e)
         else:
             right.append(e)
-    return quicksort(left, get_comparable_value) + [pivit_ele] + quicksort(right, get_comparable_value)
-
-def reverse_quicksort(arr, get_comparable_value=math.identity):
-    if len(arr) == 0 or len(arr) == 1:
-        return arr
-    pivit = random.randint(0, len(arr)-1)
-    pivit_ele = arr[pivit]
-    left = []
-    right = []
-    for e in (arr[:pivit] + arr[pivit+1:]):
-        if get_comparable_value(e) < get_comparable_value(pivit_ele):
-            left.append(e)
-        else:
-            right.append(e)
-    return reverse_quicksort(right, get_comparable_value) + [pivit_ele] + reverse_quicksort(left, get_comparable_value)
+    if reverse:
+        return quicksort(right, get_comparable_value, reverse) + [pivit_ele] + quicksort(left, get_comparable_value, reverse)
+    else:
+        return quicksort(left, get_comparable_value, reverse) + [pivit_ele] + quicksort(right, get_comparable_value, reverse)
 
