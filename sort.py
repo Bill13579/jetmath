7c83a43cde2f22244d3dff2b527eac89558ea8e9
Bill Kudo <bluesky42624@gmail.com>
2018-06-24 19:51:35 +0900

    Added reverse quicksort

--- sort.py
+++ sort.py
@@ -14,3 +14,17 @@ def quicksort(arr, get_comparable_value=math.identity):
             right.append(e)
     return quicksort(left, get_comparable_value) + [pivit_ele] + quicksort(right, get_comparable_value)
 
+def reverse_quicksort(arr, get_comparable_value=math.identity):
+    if len(arr) == 0 or len(arr) == 1:
+        return arr
+    pivit = random.randint(0, len(arr)-1)
+    pivit_ele = arr[pivit]
+    left = []
+    right = []
+    for e in (arr[:pivit] + arr[pivit+1:]):
+        if get_comparable_value(e) < get_comparable_value(pivit_ele):
+            left.append(e)
+        else:
+            right.append(e)
+    return reverse_quicksort(right, get_comparable_value) + [pivit_ele] + reverse_quicksort(left, get_comparable_value)
+
